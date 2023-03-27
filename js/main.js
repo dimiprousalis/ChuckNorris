76784adbe171274631f5e0d8ab5d847a1fbc097c
Dimitra Prousalis <dimitra.o.p@gmail.com>
2023-03-27 13:16:01 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+    const url = 'https://api.chucknorris.io/jokes/random'
+
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data)
+            document.querySelector('h3').innerText = data.value
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
+
